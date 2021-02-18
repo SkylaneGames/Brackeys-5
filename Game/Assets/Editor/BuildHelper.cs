626d9adf7deb131a0d7fe00cd0a4b4252cf0b94d
Jeremy Sapsed <jeremy@sapsed.co.uk>
2021-02-18 23:07:07 +0000

    Updated music + build settings

--- Game/Assets/Editor/BuildHelper.cs
+++ Game/Assets/Editor/BuildHelper.cs
@@ -4,7 +4,7 @@ class BuildHelper
 {
     static void PerformBuild()
     {
-        string[] scenes = { "Assets/Scenes/Splash.unity","Assets/Scenes/Menu.unity","Assets/Scenes/Game.unity" };
+        string[] scenes = { "Assets/Scenes/Splash.unity","Assets/Scenes/Menu.unity","Assets/Scenes/Game Prototype 1.unity" };
         BuildPipeline.BuildPlayer(scenes, "../build", BuildTarget.WebGL, BuildOptions.None);
     }
 }
