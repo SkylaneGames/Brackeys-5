365e7d51e11f3e8022ce455bc410ce6e09170060
Jeremy Sapsed <jms737800@gmail.com>
2021-02-18 11:01:53 +0000

    Added visibility system for spirits (#29)
    Co-authored-by: Jeremy Sapsed <jeremy@sapsed.co.uk>

---
+++ Game/Assets/Scripts/VisibilitySystem.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class VisibilitySystem : MonoBehaviour
+{
+    [SerializeField]
+    private SpiritController Target;
+
+    private List<SpiritController> AllSpirits = new List<SpiritController>();
+
+    private void ShowSpirits()
+    {
+        UpdateVisibility(true);
+    }
+
+    private void HideSpirits()
+    {
+        UpdateVisibility(false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RefreshSpiritList();
+
+        Target.PossessionSystem.CharacterPossessed += HideSpirits;
+        Target.PossessionSystem.PossessionReleased += ShowSpirits;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void RefreshSpiritList()
+    {
+        AllSpirits = FindObjectsOfType<SpiritController>().ToList();
+    }
+
+    private void UpdateVisibility(bool showSpirits)
+    {
+        Debug.Log($"Setting visibility: {showSpirits}");
+        foreach (var spirit in AllSpirits)
+        {
+            spirit.SetVisibility(showSpirits);
+        }
+    }
+}
